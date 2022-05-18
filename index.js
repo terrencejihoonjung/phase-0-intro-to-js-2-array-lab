89036c1933ca86823d62b0001b3493ed05e4738d
--replace-all <terrencejung928@gmail.com>
2022-05-17 21:20:02 -0700

    commit tree 515fc6584c86f2e5ea9f42bbc38562f960deb7ef

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = name => cats.push(name);
+
+const destructivelyPrependCat = name => cats.unshift(name);
+
+const destructivelyRemoveLastCat = () => cats.pop();
+
+const destructivelyRemoveFirstCat = () => cats.shift();
+
+const appendCat = name => {
+    const newCats = cats.slice()
+    newCats.push(name);
+    return newCats;
+}
+
+const prependCat = name => {
+    const newCats = cats.slice()
+    newCats.unshift(name);
+    return newCats;
+}
+
+const removeLastCat = function() {
+    return cats.slice(0, cats.length - 1)
+}
+
+const removeFirstCat = function() {
+    return cats.slice(1, cats.length)
+}
